cd07bd803d67607745c662c1bbcc51e5855038dd
naizareth <naizarethmedina93@gmail.com>
2023-06-06 20:07:59 +0200

    puertos

--- src/test.py
+++ src/test.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def index():
     return '¡Hola, mundo!'
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == '__main__':   
+    port = 8080
+    # Ejecuta la aplicación en el puerto especificado
+    app.run(host='0.0.0.0', port=port)
