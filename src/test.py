ed535718059d1df3b9c6cbf7dca69034ff3b6529
naizareth <naizarethmedina93@gmail.com>
2023-06-08 19:34:49 +0200

    appengine

--- src/test.py
+++ src/test.py
@@ -4,9 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola, mundo!'
-
-if __name__ == '__main__':   
-    port = 8080
-    # Ejecuta la aplicación en el puerto especificado
-    app.run(host='0.0.0.0', port=port)
+    return '¡JR is coming!'
